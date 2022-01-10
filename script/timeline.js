0190213bcda8ed4495ff79b22c2fa9d8f8af63b2
XiaHanyu <zhouyuhan_@outlook.com>
2022-01-10 15:49:02 +0800

    add timeline module

---
+++ script/timeline.js
@@ -0,0 +1,27 @@
+var global_width = $(window).width();
+var global_height = $(window).height();
+
+data = [0, 1, 2, 3, 4, 5]
+
+var sliderFill = d3
+    .sliderBottom()
+    .min(d3.min(data))
+    .max(d3.max(data))
+    .width(0.5 * global_width)
+    .tickFormat(d3.format(".2"))
+    .ticks(10)
+    .default(0.015)
+    .fill("#2196f3")
+	.on('onchange', timestamp => {
+		// 显示/隐藏节点和边
+	})
+
+var gFill = d3
+    .select("#timeline")
+    .append("svg")
+    .attr("width", global_width)
+    .attr("height", 0.15 * global_height)
+    .append("g")
+    .attr("transform", "translate(300,30)")
+
+gFill.call(sliderFill);
