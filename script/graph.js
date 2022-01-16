59b1d487fb516546c0689bb2fc1aeabaa2d591ba
duoduo-la <63342794+duoduo-la@users.noreply.github.com>
2022-01-16 21:46:34 +0800

    More interaction
    Allow users to mark nodes and show the edges' information.

--- script/graph.js
+++ script/graph.js
@@ -13,6 +13,7 @@ let chart;
 let timeline;
 const value_threshold = 5000000;
 const rate = 1e8;
+let markNode;
 
 function set_ui() {
     // 设置字体
@@ -37,6 +38,7 @@ function init() {
     ); // 2020-08-01 00:00:00
     nodeSet = new Set();
     linkSet = new Set();
+    markNode = new Set();
 
     // 得到全图全时间信息
     timeline = new Set();
@@ -75,74 +77,92 @@ function option_update(timestamp, option) {
 
     // 首先查看是否有新的节点、边加入
     // console.log(timestamp, DATA[global_index].time);
-    if (global_index > 0 && DATA[global_index - 1].time > timestamp) {
-        backward(option);
+    if(timestamp){
+        if (global_index > 0 && DATA[global_index - 1].time > timestamp) {
+            backward(option);
+        }
+
+        let new_nodes = [];
+        let new_links = [];
+        while (global_index < DATA.length) {
+            let item = DATA[global_index];
+            if (item.time > timestamp) break;
+
+            // 为 source 和 target 账户设置相应的类别
+            let g1, g2;
+            if (item.source == ADDR) {
+                g1 = 1;
+                g2 = 2;
+            } else if (item.target == ADDR) {
+                g1 = 0;
+                g2 = 1;
+            } else {
+                g1 = 2;
+                g2 = 2;
+            }
+
+            // 查看是否是图中尚没有的节点
+            if (!nodeSet.has(item.source)) {
+                nodeSet.add(item.source);
+                let c1;
+                if(markNode.has(item.source)){
+                    c1 = 3;
+                }
+                else{
+                    c1 = g1;
+                }
+                new_nodes.push({
+                    index: global_index,
+                    income: 0,
+                    outcome: 0,
+                    id: item.source,
+                    symbolSize: 5,
+                    transactions: [],
+                    input_queue: [],
+                    output_map: {},
+                    category: c1,
+                    group: g1,
+                });
+            }
+            if (!nodeSet.has(item.target)) {
+                nodeSet.add(item.target);
+                let c2;
+                if(markNode.has(item.target)){
+                    c2 = 3;
+                }
+                else{
+                    c2 = g2;
+                }
+                new_nodes.push({
+                    index: global_index,
+                    income: 0,
+                    outcome: 0,
+                    id: item.target,
+                    symbolSize: 5,
+                    transactions: [],
+                    input_queue: [],
+                    output_map: {},
+                    category: c2,
+                    group: g2,
+                });
+            }
+            // 查看是否是图中没有的边
+            if (!linkSet.has(item.source + "->" + item.target)) {
+                linkSet.add(item.source + "->" + item.target);
+                new_links.push({
+                    index: global_index,
+                    source: item.source,
+                    target: item.target,
+                    total: 0,
+                    transactions: [],
+                });
+            }
+            global_index += 1;
+        }
+
+        Array.prototype.push.apply(option.series[0].data, new_nodes);
+        Array.prototype.push.apply(option.series[0].links, new_links);
     }
-
-    let new_nodes = [];
-    let new_links = [];
-    while (global_index < DATA.length) {
-        let item = DATA[global_index];
-        if (item.time > timestamp) break;
-
-        // 为 source 和 target 账户设置相应的类别
-        let c1, c2;
-        if (item.source == ADDR) {
-            c1 = 1;
-            c2 = 2;
-        } else if (item.target == ADDR) {
-            c1 = 0;
-            c2 = 1;
-        } else {
-            c1 = 2;
-            c2 = 2;
-        }
-
-        // 查看是否是图中尚没有的节点
-        if (!nodeSet.has(item.source)) {
-            nodeSet.add(item.source);
-            new_nodes.push({
-                index: global_index,
-                income: 0,
-                outcome: 0,
-                id: item.source,
-                symbolSize: 5,
-                transactions: [],
-                input_queue: [],
-                output_map: {},
-                category: c1,
-            });
-        }
-        if (!nodeSet.has(item.target)) {
-            nodeSet.add(item.target);
-            new_nodes.push({
-                index: global_index,
-                income: 0,
-                outcome: 0,
-                id: item.target,
-                symbolSize: 5,
-                transactions: [],
-                input_queue: [],
-                output_map: {},
-                category: c2,
-            });
-        }
-        // 查看是否是图中没有的边
-        if (!linkSet.has(item.source + "->" + item.target)) {
-            linkSet.add(item.source + "->" + item.target);
-            new_links.push({
-                index: global_index,
-                source: item.source,
-                target: item.target,
-                total: 0,
-                transactions: [],
-            });
-        }
-        global_index += 1;
-    }
-
-    Array.prototype.push.apply(option.series[0].data, new_nodes);
-    Array.prototype.push.apply(option.series[0].links, new_links);
 
     // 更新所有节点和边
     function node_update(node) {
@@ -159,6 +179,13 @@ function option_update(timestamp, option) {
         }
 
         node.symbolSize = nodeScale(node.income);
+
+        if(markNode.has(node.id)){
+            node.category = 3;
+        }
+        else{
+            node.category = node.group;
+        }
 
         function input_queue_update() {
             // 更新input queue的接口
@@ -278,7 +305,7 @@ function draw_graph() {
         legend: [
             {
                 // selectedMode: 'single',
-                data: ["被骗钱包", "涉事钱包", "可能流向"],
+                data: ["被骗钱包", "涉事钱包", "可能流向", "标记钱包"],
             },
         ],
         series: [
@@ -292,6 +319,7 @@ function draw_graph() {
                     { name: "被骗钱包" },
                     { name: "涉事钱包" },
                     { name: "可能流向" },
+                    { name: "标记钱包" },
                 ],
                 roam: true,
                 // 添加箭头
@@ -315,8 +343,17 @@ function draw_graph() {
 
     chart.setOption(option);
     chart.on("click", function (params) {
-        d3.select("#record").selectAll("*").remove();
+        d3.select('#show_info').remove();
+
+        // d3.select("#record").selectAll("*").remove();
         if (params.dataType == "node") {
+            d3.select('body').append('div').attr('id','show_info').style('position','absolute')
+            .style('right','0%').style('top','0%').style('width','25%').style('height','100%');
+            // d3.select('#show_info').selectAll('*').remove();
+
+            let show_info = d3.select('#show_info');
+            show_info.append('div').attr('id','record').style('width','100%').style('height','90%').style('overflow-y','scroll').style('overflow-x','scroll')
+            show_info.append('div').attr('id','select').style('width','100%').style('height','10%');
             let width = 0.25 * $(window).width();
             let height = $(window).height();
             let entry_height = 0.05 * height;
@@ -411,9 +448,136 @@ function draw_graph() {
                             "BTC"
                     );
             }
+
+            let select_svg = d3
+                .select("#select")
+                .append("svg");
+            let select_g = select_svg.append("g")
+                .attr("transform",`translate(${width*0.7},${height*0.02})`)
+            
+            select_g.append("rect")
+                .attr("width", width*0.2)
+                .attr("height", height*0.04)
+                .attr("rx", 8)
+                .attr("stroke", "white")
+                .attr("stroke-width", 1)
+                .attr("fill", 'red')
+                .attr("fill-opacity", 0.5)
+                .on("mouseover",function(e,d){
+                    d3.select(this).attr("stroke","black");
+                })
+                .on("mouseout",function(e,d){
+                    d3.select(this).attr("stroke","white");
+                })
+                .on("click",function(e,d){
+                    if(params.data.category==3){
+                        params.data.category = params.data.group;
+                        markNode.delete(params.data.id);
+                        select_g.select('text').text("标记该钱包");
+                    }
+                    else{
+                        params.data.category = 3;
+                        markNode.add(params.data.id);
+                        select_g.select('text').text("取消该标记");
+                    }
+                    let new_option = chart.getOption();
+                    new_option = option_update(null, new_option);
+                    chart.setOption(new_option);
+                    // select_g.selectAll('*').remove();
+                });
+
+            let select_text = '标记该钱包';
+            if(params.data.category==3){
+                select_text = '取消该标记';
+            }
+            select_g.append("text")
+                .attr("dy", "2.5em")
+                .attr("x", -3)
+                .text(select_text);
+
         }
         if (params.dataType == "edge") {
-            console.log(params.data);
+            d3.select('body').append('div').attr('id','show_info').style('position','absolute')
+            .style('right','0%').style('top','0%').style('width','25%').style('height','100%');
+            // d3.select('#show_info').selectAll('*').remove();
+
+            let show_info = d3.select('#show_info');
+            show_info.append('div').attr('id','record').style('width','100%').style('height','100%').style('overflow-y','scroll').style('overflow-x','scroll')
+            // show_info.append('div').attr('id','select').style('width','100%').style('height','10%');
+
+            let width = 0.25 * $(window).width();
+            let height = $(window).height();
+            let entry_height = 0.05 * height;
+            let max_height =
+                entry_height * (5 + params.data.transactions.length);
+            let svg = d3
+                .select("#record")
+                .append("svg")
+                .attr("viewBox", "0, 0, " + width + ", " + max_height);
+            // source, target
+            let meta_datas = [
+                { source: params.data.source },
+                { target: params.data.target },
+            ];
+            for (let i = 0; i < meta_datas.length; ++i) {
+                let key = Object.keys(meta_datas[i])[0];
+                let val = meta_datas[i][key];
+                let color = "lightblue";
+                let g = svg.append("g").attr("transform", function (d, _) {
+                    return "translate(0, " + i * entry_height + ")";
+                });
+
+                g.append("rect")
+                    .attr("width", width)
+                    .attr("height", entry_height)
+                    .attr("rx", 8)
+                    .attr("stroke", "white")
+                    .attr("stroke-width", 0.8)
+                    .attr("fill", color)
+                    .attr("fill-opacity", 0.5);
+
+                g.append("text")
+                    .attr("dy", "1.5em")
+                    .text(key + ": " + val);
+            }
+            for (let i = 0; i < params.data.transactions.length; ++i) {
+                let g = svg
+                    .append("g")
+                    .attr("transform", function (d, _) {
+                        return (
+                            "translate(0, " +
+                            (i + meta_datas.length) * entry_height +
+                            ")"
+                        );
+                    })
+                    .on("mouseover", function (d) {
+						d3.select(this).select("rect").attr("stroke", "black");
+                    })
+                    .on("mouseout", function (d) {
+						d3.select(this).select("rect").attr("stroke", "white")
+                    });
+
+                let txn = params.data.transactions[i];
+                let color = "lightgreen";
+                g.append("rect")
+                    .attr("width", width)
+                    .attr("height", entry_height)
+                    .attr("rx", 8)
+                    .attr("stroke", "white")
+                    .attr("stroke-width", 1)
+                    .attr("fill", color)
+                    .attr("fill-opacity", 0.5);
+
+                g.append("text")
+                    .attr("dy", "1.5em")
+                    .attr("x", 10)
+                    .text(
+                        new Date(txn.time * 1000).toUTCString() +
+                            " " +
+                            (txn.value / rate).toFixed(4) +
+                            "BTC"
+                    );
+            }
         }
     });
 
@@ -462,10 +626,10 @@ function draw_timeline() {
     var gFill = d3
         .select("#timeline")
         .append("svg")
-        .attr("width", global_width)
+        .attr("width", global_width*0.75)
         .attr("height", 0.15 * global_height)
         .append("g")
-        .attr("transform", "translate(300,30)");
+        .attr("transform", `translate(${global_width*0.75*0.1},${global_height*0.15*0.5})`);
 
     gFill.call(sliderFill);
 }
